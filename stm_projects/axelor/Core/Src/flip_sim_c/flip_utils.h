6e923bad9d277e877a15162b56d75952835e863c
Jeewantha H.M.S <shamilajeewantha1@gmail.com>
2026-06-25 08:02:29 +0530

    removed old folders

--- stm_projects/axelor/Core/Src/flip_sim_c/flip_utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef FLIP_UTILS_H
-#define FLIP_UTILS_H
-
-#include "scene.h"
-
-// Setup a generic scene; takes a Scene pointer as argument
-void setupScene(Scene *scene);
-
-#endif
--- tutorial/README.md
+++
@@ -1,2 +0,0 @@
-gcc -o book_system book_system.c
-./book_system
